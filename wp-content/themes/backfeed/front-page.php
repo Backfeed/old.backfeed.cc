d2e9eb8837e6d5887d78e77c7224677b0fa590bf
Ore Landau <Ore4444@gmail.com>
2015-08-11 17:30:36 +0300

    initial commit

---
+++ wp-content/themes/backfeed/front-page.php
@@ -0,0 +1,61 @@
+<div class="col-xs-12">
+
+    <section id="about" class="with-illustration">
+        <div class="illustration">
+            <img src="wp-content/themes/backfeed/assets/images/compass.png" alt="Compass">
+        </div>
+        <div class="text">
+            <h2>About Backfeed</h2>
+            <p class="lead">Backfeed make large-scale collaboration possible. Based on blockchain technology, our protocols enable people to create and maintain decentralized collaboration (DCs) in an organic and distributed manner, independent of any central governance structure.</p>
+            <p>Backfeed develops infrastructure and tools that build on the emerging blockchain ecosystem. Providing resilient technology and innovative economic models to promote viable and sustainable scale-free organizations through equitable distribution of co-created value. Our generic API can be easily integrated with any application or online tool.</p>
+            <p>As an analogy, if the blockchain can be regarded as the TCP/IP (communication layer of the internet), Backfeed is building a canonical protocol and platform (akin to the HTTP protocol and the web browser) to enable blockchain-based decentralized collaboration, as easily as one would deploy a website today. Think of Backfeed as the WordPress for decentralized applications and the GitHub for decentralized organisations.</p>
+        </div>
+    </section>
+
+    <aside class="tagline"></aside>
+
+    <section id="vision" class="with-illustration">
+        <div class="illustration">
+            <img src="wp-content/themes/backfeed/assets/images/woman_with_idea.png" alt="Woman with Idea">
+        </div>
+        <div class="text">
+            <h2>Our Vision</h2>
+            <p>Imagine Facebook owned by its users. Imagine people lending or borrowing money, buying and selling goods or services without relying on banks or Paypal, insuring each other directly without intermediate insurance companies.</p>
+            <p>Imagine a decentralized transportation network independent of Uber, renting apartments without transaction fees, new P2P marketplaces emerging as alternatives to eBay and Amazon.</p>
+            <p>Imagine open source communities where contributors are also shareholders, and in which the creation of value leads to its fair and transparent distribution.</p>
+            <p>Imagine corporations that spark off extraordinary contributions of individuals and unleash innovation hindered by the rigidities of hierarchical structures.</p>
+            <p>With Backfeed, this vision is beginning to unfold...</p>
+        </div>
+    </section>
+
+    <aside class="tagline"></aside>
+
+    <section id="use-cases" class="only-text"></section>
+
+    <aside class="tagline"></aside>
+
+    <section id="technology" class="with-illustration">
+        <div class="illustration">
+            <img src="wp-content/themes/backfeed/assets/images/technology.png" alt="Technology">
+        </div>
+        <div class="text">
+            <h2>Technology</h2>
+            <p>Backfeed develops foundational tools for decentralized collaboration, syncing the spontaneous actions of millions of people to promote an era of collaboration and decentralized value production. The technological tools support:</p>
+            <ul>
+                <li>Decentralized Governance</li>
+                <li>Fair Value Distribution</li>
+                <li>Bottom-up Decision Making</li>
+                <li>Incentivisation Models</li>
+            </ul>
+            <p>In particular, Backfeed is currently working on developing two foundational tools for decentralized collaboration:</p>
+            <h3>The Backfeed protocols</h3>
+            <p>a set of rules for the distribution of value among contributors to a decentralized organisation, including economic tokens and reputational rewards.</p>
+            <h3>The visual browser</h3>
+            <p>a navigation system for people to immediately understand the structure of decentralized organisations, so they see how they can contribute the most at any given time.</p>
+        </div>
+    </section>
+
+    <section id="team"></section>
+
+    <section id="partners"></section>
+</div>
