b8cbd9481612390ba6dc5af24088b9763dfe4fcb
Ore Landau <Ore4444@gmail.com>
2015-09-06 18:23:55 +0300

    added press section

--- wp-content/themes/backfeed/front-page.php
+++ wp-content/themes/backfeed/front-page.php
@@ -382,7 +382,7 @@
                             <div class="team-member-role">Chief Designer</div>
                         </div>
                         <div class="back">
-                            Creating experiences with three open eyes
+                            Creating experiences with three eyes open
                             <div class="team-member-social-icons">
                                 <a href="https://www.linkedin.com/in/amnonbarnea" target="_blank"><i class="fa fa-linkedin-square"></i></a>
                                 <a href="https://www.facebook.com/AmnonBarneaDesign" target="_blank"><i class="fa fa-facebook-official"></i></a>
@@ -445,3 +445,23 @@
     </div>
 
 </section>
+
+<div class="container">
+    <section id="press">
+
+        <div class="press-item">
+            <div class="press-title">
+                <a href="http://www.geektime.com/2015/08/19/backfeed-wants-to-decentralize-the-internet-and-help-you-earn-what-you-deserve/">
+                    <img class="media-object" src="/wp-content/themes/backfeed/assets/images/press/geektime.png" alt="...">
+                </a>
+            </div>
+            <div class="press-body">
+                <a href="http://www.geektime.com/2015/08/19/backfeed-wants-to-decentralize-the-internet-and-help-you-earn-what-you-deserve/">
+                    <h4 class="media-heading">Backfeed wants to decentralize the Internet and help you earn what you deserve</h4>
+                </a>
+                Founded by a small group of graduate students with a vision for a fairer society, this startup aims to start a revolution as big as the Internet itself
+            </div>
+        </div>
+
+    </section>
+</div>
