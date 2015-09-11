1b44f998bd5af6da7182dc01808006782c28b0c5
Ore Landau <Ore4444@gmail.com>
2015-09-11 12:07:20 +0300

    layout change in header

--- wp-content/themes/backfeed/templates/head.php
+++ wp-content/themes/backfeed/templates/head.php
@@ -9,9 +9,5 @@ use Roots\Sage\Titles;
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta property="og:title" content="<?=wp_title()?>" />
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content="<?=Utils\get_current_page_url()?>" />
-  <meta property="og:image" content="<?=site_url()?>/wp-content/themes/backfeed/assets/images/open-graph-image.png" />
-  <?php wp_head(); ?>
+   <?php wp_head(); ?>
 </head>
--- wp-content/themes/backfeed/templates/header.php
+++ wp-content/themes/backfeed/templates/header.php
@@ -8,21 +8,23 @@
                         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills']);
                 endif; ?>
 
+            <div class="header-contact-methods">
                 <div id="header-subscribe" class="morph-button morph-button-modal morph-button-modal-2 morph-button-fixed">
                     <button class="btn btn-primary" type="button">Subscribe</button>
                     <div class="morph-content"><div>
-                        <div class="content-style-form content-style-form-1">
-                            <span class="icon-close glyphicon glyphicon-remove"></span>
-                            <?php mc4wp_form(71); ?>
-                        </div>
-                    </div></div>
+                            <div class="content-style-form content-style-form-1">
+                                <span class="icon-close glyphicon glyphicon-remove"></span>
+                                <?php mc4wp_form(71); ?>
+                            </div>
+                        </div></div>
                 </div>
 
                 <div class="social-icons">
-                        <a href="http://linkedin.com/company/backfeed" target="_blank"><i class="fa fa-linkedin"></i></a>
-                        <a href="http://twitter.com/backfeed_cc" target="_blank"><i class="fa fa-twitter"></i></a>
-                        <a href="mailto:info@backfeed.cc"><i class="fa fa-envelope"></i></a>
+                    <a href="http://linkedin.com/company/backfeed" target="_blank"><i class="fa fa-linkedin"></i></a>
+                    <a href="http://twitter.com/backfeed_cc" target="_blank"><i class="fa fa-twitter"></i></a>
+                    <a href="mailto:info@backfeed.cc"><i class="fa fa-envelope"></i></a>
                 </div>
+            </div>
 
                 <!--        <div id="collaborative-tagline-wrapper" class="row">-->
                 <?php ////the_widget('Tagline_Widget'); ?>
